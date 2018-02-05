8c9f6a966e7a2966407e17f59997227a92c42031
MarianaTsV <marianatsv@gmail.com>
2018-02-05 13:43:49 +0200

    list

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -12,18 +12,24 @@ namespace Snake
         {
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
-            /* int x1 = 1;
-             int y1 = 3;
-             char sym1 = '*';
 
-             Draw(x1, y1, sym1);*/
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-            /* int x2 = 4;
-             int y2 = 5;
-             char sym2 = '#';
 
-             Draw(x2, y2, sym2);*/
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+
+            foreach (var item in numList)
+            {
+                Console.WriteLine(item);
+            }
 
             Console.ReadLine();
         }
